afe6fdec1d177c6f303f3e3cdc2fd476545369ef
Eric Clack <eric@bn7.net>
2020-02-16 11:42:35 +0000

    Start of version 2

---
+++ pacman2.py
@@ -0,0 +1,103 @@
+WORLD_SIZE = 20
+BLOCK_SIZE = 32
+WIDTH = WORLD_SIZE*BLOCK_SIZE
+HEIGHT = WORLD_SIZE*BLOCK_SIZE
+
+# An array containing the world tiles
+world = []
+
+# Our sprites
+pacman = Actor('pacman_o.png', anchor=('left', 'top'))
+pacman.x = pacman.y = 1*BLOCK_SIZE
+# Direction that we're going in
+pacman.dx, pacman.dy = 0,0
+
+ghosts = []
+
+# Your level will contain characters, they map
+# to the following images
+char_to_image = {
+    '.': 'dot.png',
+    '=': 'wall.png',
+    '*': 'power.png',
+    'g': 'ghost1.png',
+    'G': 'ghost2.png',
+}
+
+def load_level(number):
+    file = "level-%s.txt" % number
+    with open(file) as f:
+        for line in f:
+            row = []
+            for block in line.strip():
+                row.append(block)
+            world.append(row)
+
+def make_ghost_actors():
+    for y, row in enumerate(world):
+        for x, block in enumerate(row):
+            if block in ['g', 'G']:
+                g = Actor(char_to_image[block], (x*BLOCK_SIZE, y*BLOCK_SIZE), anchor=('left', 'top'))
+                ghosts.append(g)
+                # Now we have the ghost sprite we don't need this block
+                world[y][x] = None
+
+def draw():
+    screen.clear()
+    for y, row in enumerate(world):
+        for x, block in enumerate(row):
+            image = char_to_image.get(block, None)
+            if image:
+                screen.blit(char_to_image[block], (x*BLOCK_SIZE, y*BLOCK_SIZE))
+    pacman.draw()
+    for g in ghosts: g.draw()
+
+def blocks_ahead_of_pacman(dx, dy):
+    """Return a list of tiles at this position + delta"""
+
+    # Here's where we want to move to
+    x = pacman.x + dx
+    y = pacman.y + dy
+
+    # Find integer block pos, using floor (so 4.7 becomes 4)
+    ix,iy = int(x // BLOCK_SIZE), int(y // BLOCK_SIZE)
+    # Remainder let's us check adjacent blocks
+    rx, ry = x % BLOCK_SIZE, y % BLOCK_SIZE
+
+    blocks = [ world[iy][ix] ]
+    if rx: blocks.append(world[iy][ix+1])
+    if ry: blocks.append(world[iy+1][ix])
+    if rx and ry: blocks.append(world[iy+1][ix+1])
+
+    return blocks
+
+def update():
+    # In order to go in direction dx, dy their must be no wall that way
+    if '=' not in blocks_ahead_of_pacman(pacman.dx, 0):
+        pacman.x += pacman.dx
+    if '=' not in blocks_ahead_of_pacman(0, pacman.dy):
+        pacman.y += pacman.dy
+
+    #if '=' not in blocks_ahead_of_pacman(pacman.dx, pacman.dy):
+    #    pacman.x += pacman.dx
+    #    pacman.y += pacman.dy
+
+def on_key_up(key):
+    if key in (keys.LEFT, keys.RIGHT):
+        pacman.dx = 0
+    if key in (keys.UP, keys.DOWN):
+        pacman.dy = 0
+
+def on_key_down(key):
+    if key == keys.LEFT:
+        pacman.dx = -2
+    if key == keys.RIGHT:
+        pacman.dx = 2
+    if key == keys.UP:
+        pacman.dy = -2
+    if key == keys.DOWN:
+        pacman.dy = 2
+
+# Game set up
+load_level(1)
+make_ghost_actors()
